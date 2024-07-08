722795f823e058df55db2031f6ac17bfe555bde7
Dat Huu Nguyen <99190085+DatNguyen2003@users.noreply.github.com>
2024-07-07 23:13:10 -0400

    Delete reset_db.sql

--- reset_db.sql
+++
@@ -1,5 +0,0 @@
--- Delete all data from the keys_attributes table
-DELETE FROM keys_attributes;
-
--- Reset the auto-increment value of the attribute_id column
-ALTER TABLE keys_attributes AUTO_INCREMENT = 1;
